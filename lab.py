6dd61b5aa322437352c60157a8b8c5c97c2d824c
Honeybee1023 <93350084+Honeybee1023@users.noreply.github.com>
2025-11-20 12:30:53 -0800

    Update lab.py
    Improved formatting

--- lab.py
+++ lab.py
@@ -17,12 +17,14 @@ from scheme_utils import (
     # SchemeSyntaxError, # uncomment in LISP part 2!
     SchemeREPL,
 )
+
 sys.setrecursionlimit(20_000)
 # NO ADDITIONAL IMPORTS!
 
 ####################################################################
 # region                  Tokenization
 ####################################################################
+
 
 def tokenize(source):
     r"""
@@ -41,37 +43,39 @@ def tokenize(source):
     current_token = ""
     comment = False
     for ch in source:
-        #ignore comments without doing anything
+        # ignore comments without doing anything
         if comment:
-            if ch == '\n':
+            if ch == "\n":
                 comment = False
             continue
 
-        #; marks the start of a comment
-        if ch == ';':
+        # ; marks the start of a comment
+        if ch == ";":
             comment = True
             continue
-        #each token ends with a space of some sort or paren
-        elif ch == " " or ch == "\n" or ch == "\t":
+        # each token ends with a space of some sort or paren
+        elif ch in  (" ", "\n", "\t"):
             if current_token != "":
                 tokens.append(current_token)
                 current_token = ""
-        elif ch == '(' or ch == ')':
+        elif ch in ("(", ")"):
             if current_token != "":
                 tokens.append(current_token)
                 current_token = ""
             tokens.append(ch)
         else:
             current_token += ch
-    #add the last token if there is one
+    # add the last token if there is one
     if current_token != "":
         tokens.append(current_token)
     return tokens
+
 
 # endregion
 ####################################################################
 # region                  Parsing
 ####################################################################
+
 
 def parse(tokens):
     """
@@ -92,19 +96,19 @@ def parse(tokens):
     if not tokens:
         raise SchemeEvaluationError("Empty Tokens")
 
-    token = tokens.pop(0) #shorten the list of tokens each time
+    token = tokens.pop(0)  # shorten the list of tokens each time
 
-    if token == '(':
+    if token == "(":
         current_expr = []
-        while tokens[0] != ')':
-            #recursively parse the stuff inside this layer of paren 
-            current_expr.append(parse(tokens)) 
-            #unmatched paren when we reach end without )
+        while tokens[0] != ")":
+            # recursively parse the stuff inside this layer of paren
+            current_expr.append(parse(tokens))
+            # unmatched paren when we reach end without )
             if not tokens:
                 raise SchemeEvaluationError("Unmatched parenthesis")
         tokens.pop(0)
         return current_expr
-    elif token == ')':
+    elif token == ")":
         raise SchemeEvaluationError("Unmatched parenthesis")
     else:
         return number_or_symbol(token)
@@ -115,7 +119,8 @@ def parse(tokens):
 # region                       Evaluation
 ####################################################################
 
-def evaluate(tree, frame = None):
+
+def evaluate(tree, frame=None):
     """
     Given tree, a fully parsed expression, evaluates and outputs the result of
     evaluating expression according to the rules of the Scheme language.
@@ -129,39 +134,39 @@ def evaluate(tree, frame = None):
         frame = make_initial_frame()
     if not isinstance(tree, list):
         if isinstance(tree, (int, float)):
-            #if number, just return number
+            # if number, just return number
             return tree
         else:
-            #if symbol, return object associated with symbol
+            # if symbol, return object associated with symbol
             if frame.is_defined(tree):
                 return frame.lookup(tree)
             else:
                 raise SchemeNameError("Symbol", tree, "not found/undefined")
     else:
-        #evaluate([3.14]) should raise a SchemeEvaluationError because a float is not callable.
+        # evaluate([3.14]) should raise a SchemeEvaluationError, float not callable.
         if not isinstance(tree[0], str):
             if not isinstance(tree[0], list):
                 raise SchemeEvaluationError("Operation not callable")
-        #evaluate(['a', 1, 2]), should raise a SchemeNameError
+        # evaluate(['a', 1, 2]), should raise a SchemeNameError
         elif tree[0] not in SCHEME_BUILTINS and not frame.is_defined(tree[0]):
             raise SchemeNameError("Symbol not defined")
         oper = evaluate(tree[0], frame)
-        #specially handle the define operation
+        # specially handle the define operation
         if oper == "define":
             args = tree[1:]
             if len(args) < 2:
                 raise SchemeEvaluationError("Define needs 2 arguments")
             var = args[0]
             val = args[1]
-            if isinstance(var, list): #simple function definition
+            if isinstance(var, list):  # simple function definition
                 func_name = var[0]
                 params = var[1:]
                 body = val
                 function = Function(params, body, frame)
                 frame.define(func_name, function)
                 return function
-            else: #normal function or var definition
-                #if val is expression or other variable name, need to evaluate first
+            else:  # normal function or var definition
+                # if val is expression or other variable name, need to evaluate first
                 if not isinstance(val, (int, float)):
                     val = evaluate(val, frame)
                 frame.define(var, val)
@@ -179,12 +184,11 @@ def evaluate(tree, frame = None):
             raise SchemeEvaluationError("Function cannot be completed with given args")
 
 
-
-
 # endregion
 ####################################################################
 # region                      Built-ins
 ####################################################################
+
 
 def builtin_mul(*args):
     """
@@ -225,10 +229,11 @@ class Frame:
     Frames can have variables defined in them, lookup variables
     in this frame or parent frames.
     """
+
     def __init__(self, parent_frame=None):
         self.parent_frame = parent_frame
         self.mapping = {}
-    
+
     def define(self, symbol, value):
         """
         Give this symbol a value in the mapping in this frame.
@@ -246,7 +251,7 @@ class Frame:
             return self.parent_frame.lookup(symbol)
         else:
             raise SchemeNameError("Symbol undefined")
-        
+
     def is_defined(self, symbol):
         """
         Returns True if this symbol is defined in this frame or any parent frame.
@@ -256,10 +261,11 @@ class Frame:
             return True
         except SchemeNameError:
             return False
-        
+
     def __str__(self):
         return f"Frame({self.mapping})"
-        
+
+
 def make_initial_frame():
     """
     Creates and returns a Frame with the built-in Scheme functions defined.
@@ -270,6 +276,8 @@ def make_initial_frame():
     for symbol, func in SCHEME_BUILTINS.items():
         parent_frame.define(symbol, func)
     return frame
+
+
 # endregion
 # region                  User Functions
 ####################################################################
@@ -277,6 +285,7 @@ class Function:
     """
     A user-defined Scheme function.
     """
+
     def __init__(self, parameters, body, defining_frame):
         self.parameters = parameters
         self.body = body
@@ -286,12 +295,14 @@ class Function:
         if len(args) != len(self.parameters):
             raise SchemeEvaluationError("Incorrect number of arguments")
 
-        #in frame of this fuction, set params to user input
-        new_frame = Frame(self.defining_frame) #lexical scoping
+        # in frame of this fuction, set params to user input
+        new_frame = Frame(self.defining_frame)  # lexical scoping
         for param, arg in zip(self.parameters, args):
             new_frame.define(param, arg)
 
         return evaluate(self.body, new_frame)
+
+
 # endregion
 # region                       REPL
 ####################################################################
@@ -303,10 +314,12 @@ if __name__ == "__main__":
     if run_doctest:
         _doctest_flags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
         doctest.run_docstring_examples(tokenize, globals(), optionflags=_doctest_flags)
-        #doctest.testmod(optionflags=_doctest_flags)  # runs ALL doctests
+        # doctest.testmod(optionflags=_doctest_flags)  # runs ALL doctests
 
     if run_repl:
         sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
-        SchemeREPL(sys.modules[__name__], verbose=True, repl_frame=make_initial_frame()).cmdloop()
+        SchemeREPL(
+            sys.modules[__name__], verbose=True, repl_frame=make_initial_frame()
+        ).cmdloop()
 
 # endregion
